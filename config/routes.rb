4000396768d38ef21c2f44c61588700b86dfeb36
yuxing <yuxing0129@gmail.com>
2015-10-08 17:21:37 +0900

    Added contact page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'pages#welcome'
   get 'pages/portfolio' => 'pages#portfolio', as: 'portfolio'
   get 'pages/about' => 'pages#about', as: 'about'
+  get 'pages/contact' => 'pages#contact', as: 'contact'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
